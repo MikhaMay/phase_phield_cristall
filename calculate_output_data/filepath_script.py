f82a9a943eaccf40fe2f9a142210dc4083dc9d27
MikhaMay <mishamaifet@yandex.ru>
2025-05-02 19:45:23 +0300

    com data

---
+++ calculate_output_data/filepath_script.py
@@ -0,0 +1,45 @@
+import os
+
+
+# Максимальное значение для int32
+# INT32_MAX = 2**31 - 1
+# INT32_MIN = -2**31 - 1
+
+# print(INT32_MAX)
+
+# 2100000000
+# 2150000000
+
+# num = -2144967296
+
+# result = num % (2**32)
+
+num_max = 2**32
+
+# result = INT32_MAX + (-2144967296 - INT32_MIN)
+
+# print(result)
+
+# Путь к директории
+directory_path = 'calculate_output_data/Lx20.0_Ly20.0_Nx64_Ny64_eps0.40_dt1.0e-08_Periodic_2d_random_-0.3avg'
+
+numbers = []
+# Проход по всем файлам в директории
+for filename in os.listdir(directory_path):
+    if filename in ('params.yaml', 'energies.bin'):
+        continue
+    file_number = int(filename.removesuffix('.bin'))
+    
+    if file_number % 10 != 0:
+        file_number = file_number % num_max
+
+        if file_number % 10 != 0:
+            file_number += num_max
+
+    old_file_path = os.path.join(directory_path, filename)
+    new_file_path = os.path.join(directory_path, f'{file_number}.bin')
+
+    os.rename(old_file_path, new_file_path)
+    print(f'Renamed: {old_file_path} -> {new_file_path}')
+
+
